a5c722998f596b71db648033bf1e952398a6ef11
adrianadducchio <adrian.vhvhvh@gmail.com>
2022-10-12 17:31:14 -0300

    form keepers

---
+++ public/javascripts/formKeepers.js
@@ -0,0 +1,36 @@
+const scriptURL =
+  "https://script.google.com/macros/s/AKfycbx_lJPUoOfkUJOOzFigs8Q4ms_P18fa-4ytFRRvXolFiUjdLARlWeyMl7NkiB7lQVrZ0Q/exec";
+
+function handleSubmit() {
+  const inputName = document.getElementById("name").value;
+  const inputPhone = document.getElementById("phone").value;
+  const inputYearOfBirth = document.getElementById("yearOfBirth").value;
+  const inputCity = document.getElementById("city").value;
+
+  const form = document.forms["dataForm"];
+  const info = document.getElementById("info");
+  const icon = `<img class='icon-info' src='../images/icons/info.svg' alt='icon-info' />`;
+
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();    
+    if (
+      inputName === "" ||
+      inputPhone === "" ||
+      inputYearOfBirth === "" ||
+      inputCity === ""
+    ) {
+      return (info.innerHTML = `${icon} Todos los datos deben estar completos.`);
+    }
+    info.innerHTML = `${icon} Tus datos estan siendo enviados...`;
+
+    fetch(scriptURL, { method: "POST", body: new FormData(form) })
+      .then(() => {
+        info.innerHTML = `${icon} Tus datos fueron enviados, pronto estaremos en contacto contigo.`;
+        form.reset();
+      })
+      .catch(
+        () =>
+          (info.innerHTML = `${icon} Sucedió un error al intentar contactar, puedes comunicar via telefono.`)
+      );
+  });
+}
